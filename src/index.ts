8a0c06f1e44a5cca603dbdd838d693b45cfbec39
Mees van Dijk <mees@mees.io>
2023-01-09 16:33:00 +0100

    Add logs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config"
 import { WebSocketServer, WebSocket } from "ws"
+import logger from "./log.js"
 
 const PORT = isNaN(parseInt(process.env.PORT ?? "")) ? 80 : parseInt(process.env.PORT as string)
 
@@ -24,18 +25,24 @@ type WsMessgaeMessage = {
 }
 
 const wss = new WebSocketServer({ port: PORT })
+logger.info({ port: PORT }, `server started on port ${PORT}`)
 
 wss.on("connection", ws => {
+  logger.trace("new connection")
   ws.on("message", message => {
+    logger.trace({ message: message.toString() }, "new message")
     try {
       const data: WsMessage = JSON.parse(message.toString())
       const { channel, type } = data
       if (type === "open") {
+        logger.trace({ channel }, "open channel")
         channelListeners[channel] = channelListeners[channel] ?? []
         channelListeners[channel].push(ws)
       } else if (channelListeners[channel] == null) {
+        logger.error({ channel }, "channel not found")
         ws.send(JSON.stringify({ type: "error", payload: "channel not found" }))
       } else if (type === "close") {
+        logger.trace({ channel }, "close channel")
         for (const sock of channelListeners[channel]) {
           sock.send(JSON.stringify({ type: "close" }))
           sock.close()
@@ -43,6 +50,7 @@ wss.on("connection", ws => {
         delete channelListeners[channel]
       } else if (type === "message") {
         const { payload } = data
+        logger.trace({ channel, payload }, "new message in channel")
         for (const sock of channelListeners[channel]) {
           if (sock !== ws) {
             sock.send(JSON.stringify({ type: "message", payload }))
